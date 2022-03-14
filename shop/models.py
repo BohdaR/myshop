574ac8e27d8462528f1eb14cb71522eda790e99f
bodya <bogdanshushval25@gmail.com>
2022-03-14 08:01:40 +0200

    create cart

---
+++ shop/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Назва категорії')
+    slug = models.SlugField(max_length=200, db_index=True, unique=True, verbose_name='URL')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'категорію'
+        verbose_name_plural = 'Категорії'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_list_by_category',
+                        args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.PROTECT, verbose_name = 'Категорія товару')
+    name = models.CharField(max_length=200, db_index=True, verbose_name = 'Назва товару')
+    slug = models.SlugField(max_length=200, db_index=True, unique=True, verbose_name = 'URL')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name = 'Зображення товару')
+    description = models.TextField(blank=True, verbose_name = 'Опис товару')
+    old_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name = 'Ціна до знижки')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name = 'Ціна товару')
+    stock = models.PositiveIntegerField(verbose_name = 'Залишок товару')
+    available = models.BooleanField(default=True, verbose_name = 'Доступність товару на складі')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+        verbose_name = 'товар'
+        verbose_name_plural = 'Товари'
+        ordering = ['-updated', '-created', '-id']
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_detail',
+                        args=[self.slug])
