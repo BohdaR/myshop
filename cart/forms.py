80fd7e86ca1fa4b6df4fa8ce2a977884637d89c5
bodya <bogdanshushval25@gmail.com>
2022-03-15 14:10:57 +0200

    rm file

--- cart/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class CartAddProductForm(forms.Form):
-    quantity = forms.IntegerField(initial=1, min_value=0, max_value=21)
-    update = forms.BooleanField(required=False,
-                                initial=True,
-                                widget=forms.HiddenInput)
