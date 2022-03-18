53bb530ae8898a0dfef9ded6e8de43006c81b4c6
bodya <bogdanshushval25@gmail.com>
2022-03-18 11:44:02 +0200

    changed slider

--- shop/static/shop/js/script.js
+++ shop/static/shop/js/script.js
@@ -1,36 +1,31 @@
-let slider_item = document.getElementsByClassName('slider-item');
-let slider = document.querySelectorAll('.slider-item-list');
+let slides = document.querySelectorAll('.slide-img');
 let Left = document.getElementsByClassName('Left');
 let Right = document.getElementsByClassName('Right');
-let sld_mrg = 0
+let i = 0
 
-function left_change_slide() {
-    sld_mrg -=500
-    switch (sld_mrg) {
-        case slider_item.length * - 500 :
-            sld_mrg = 0;
-          break;
-        case 500:
-          sld_mrg = -slider_item.length * 500 + 500;
-          break;
-      }
-      console.log(sld_mrg)
-      slider[0].style = `margin-left: ${sld_mrg}px`;
+
+console.log(0)
+slides[0].style.opacity = 1;
+
+
+function left_next_slide() {
+  slides[i].style.opacity = 0;
+  i++;
+  if (i == slides.length) {
+    i = 0;
+  }
+  slides[i].style.opacity = 1;
 }
 
-function right_change_slide() {
-    sld_mrg +=500
-    switch (sld_mrg) {
-        case slider_item.length * -500 :
-            sld_mrg = 0;
-          break;
-        case 500:
-          sld_mrg = slider_item.length * -500 + 500;
-          break;
-      }
-      console.log(sld_mrg)
-      slider[0].style = `margin-left: ${sld_mrg}px`;
+function right_next_slide() {
+  slides[i].style.opacity = 0;
+  if (i == 0) {
+    i = slides.length;   
+  }
+  i--;
+  console.log(i)
+  slides[i].style.opacity = 1;
 }
 
-Left[0].addEventListener("click", left_change_slide);
-Right[0].addEventListener("click", right_change_slide);
+Left[0].addEventListener("click", left_next_slide);
+Right[0].addEventListener("click", right_next_slide);
