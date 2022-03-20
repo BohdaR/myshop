821eb8f7db640c796fe0e0ea35d527be5c950c00
bogdan <bogdanshushval25@gmail.com>
2022-03-20 21:20:14 +0200

    creared custom order page

--- orders/forms.py
+++ orders/forms.py
@@ -5,8 +5,22 @@ from .models import Order
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'address', 'phone_number', 'city', 'delivery_method', 'pay_method']
+        fields = ['first_name', 'last_name', 'email', 'city', 'address', 'phone_number', 'delivery_method', 'pay_method']
         widgets = {
-            'delivery_method': forms.RadioSelect(attrs={'class': 'order-chekbox'}),
-            'pay_method': forms.RadioSelect(attrs={'class': 'order-chekbox'}),
+            'first_name':forms.TextInput(attrs={
+                'placeholder': 'Ім\'я'}),
+            'last_name':forms.TextInput(attrs={
+                'placeholder': 'Прізвище'}),
+            'email':forms.TextInput(attrs={
+                'placeholder': 'Eмейл'}),
+            'address':forms.TextInput(attrs={
+                'placeholder': 'Адреса доставки'}),
+            'phone_number':forms.TextInput(attrs={
+                'placeholder': 'Номер телефону'}),
+            'city':forms.TextInput(attrs={
+                'placeholder': 'Ваше місто'}),
+            'delivery_method': forms.RadioSelect(attrs={
+                'class': 'order-chekbox'}),
+            'pay_method': forms.RadioSelect(attrs={
+                'class': 'order-chekbox'}),
         }
