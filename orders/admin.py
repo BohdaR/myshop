29a1a670c11f420a3f60e197adae5eb6e77f5775
bogdan <bogdanshushval25@gmail.com>
2022-03-20 13:27:35 +0200

    chenged order page

--- orders/admin.py
+++ orders/admin.py
@@ -9,10 +9,11 @@ class OrderItemInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
-                    'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'address', 'phone_number', 'city', 'paid',
+                    'created', 'updated','pay_method', 'delivery_method','pay_method']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
 
 
 admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
--- orders/forms.py
+++ orders/forms.py
@@ -5,4 +5,8 @@ from .models import Order
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        fields = ['first_name', 'last_name', 'email', 'address', 'phone_number', 'city', 'delivery_method', 'pay_method']
+        widgets = {
+            'delivery_method': forms.RadioSelect(attrs={'class': 'order-chekbox'}),
+            'pay_method': forms.RadioSelect(attrs={'class': 'order-chekbox'}),
+        }
